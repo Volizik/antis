94c02e8b9ed987b5271bc2eaa1943eed273b4602
kavalar <apuc06@mail.ru>
2018-04-06 12:58:50 +0300

    add responsive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,31 @@
 (function ($) {
 	$(() => {
         $('.about__slider-wrap').slick({
-            slidesToShow: 1
+            slidesToShow: 1,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
         });
     });
+
+	$(document).on('click', '.nav__button', function () {
+        if ($(this).siblings('.nav__wrap').hasClass('is-flex')) {
+            $(this).siblings('.nav__wrap').hide().removeClass('is-flex');
+        }else {
+            $(this).siblings('.nav__wrap').slideDown().addClass('is-flex');
+        }
+        $(this).toggleClass('nav__button--clicked');
+    });
+
 })(jQuery);
